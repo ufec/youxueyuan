318ccbf34991a5ebad95bf0d1b0ffdf4c4baad18
Chrise <34962267+Chirmis@users.noreply.github.com>
2020-03-28 14:53:42 +0800

    更新

--- shuake.py
+++ shuake.py
@@ -1,11 +1,13 @@
-import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.parse
 import http.cookiejar
 import requests
 import json
 import os
+import re
 from moviepy.editor import VideoFileClip
 import time
 import webbrowser
+import random
 
 def login():
     global Token
@@ -30,7 +32,7 @@ def login():
     for item in cookie:
         Token[item.name] = urllib.parse.unquote(item.value)
     if 'AUTHORIZATION' in Token and 'token' in Token and 'UMOOC_SESSION' in Token:
-        with open('cookies.txt', 'wb', True) as f:
+        with open('./cookies.txt', 'wb', True) as f:
             f.write(json.dumps(Token).encode('UTF-8'))
         headers = {
             'UA-AUTHORIZATION': Token['token'],
@@ -39,7 +41,7 @@ def login():
         }
         return("登陆成功...")
     else:
-        print("用户名或密码错误，登陆失败，请重新运行程序.....")
+        print("用户名或密码错误，登陆失败")
         login()
 
 def get_courses_list():
@@ -47,7 +49,7 @@ def get_courses_list():
     get_courses_list_api = "https://courseapi.ulearning.cn/courses/students?keyword=&publishStatus=1&type=1&pn=1&ps=15"
     response = requests.request(method='GET', url=get_courses_list_api, headers=headers)
     if 'code' in response.json() and response.json()['code'] == 2101:
-        with open('cookies.txt', 'wb', True) as f:
+        with open('./cookies.txt', 'wb', True) as f:
             f.truncate()
         print("身份已过期请重新登陆")
         login()
@@ -85,7 +87,7 @@ def get_chapters_item(textbookId, classId):
     response = requests.request(method='GET', url=get_directory_info_api, headers=headers)
     return response.json()
 
-def get_every_section(nodeid):
+def get_every_section(nodeid, itemsInfo):
     global headers
     get_every_section_api = "https://api.ulearning.cn/wholepage/chapter/stu/%d" % nodeid
     response = requests.request(method='GET', url=get_every_section_api, headers=headers)
@@ -95,7 +97,7 @@ def get_every_section(nodeid):
     userInfoDict = response2.json()
     k = 0
     while k < len(sectionDict['wholepageItemDTOList']):
-        check_section(sectionDict['wholepageItemDTOList'][k], userInfoDict)
+        check_section(sectionDict['wholepageItemDTOList'][k], userInfoDict, itemsInfo[k])
         k += 1
 
 def check_answer(coursepageDTOList):
@@ -106,15 +108,12 @@ def check_answer(coursepageDTOList):
     questionsData = []
     i = 0
     while i < len(coursepageDTOList):
-        j = 0
-        while j < len(coursepageDTOList[i]):
-            if coursepageDTOList[i]['questionDTOList']:
-                k = 0
-                while k < len(coursepageDTOList[i]['questionDTOList']):
-                    questionid = coursepageDTOList[i]['questionDTOList'][k]['questionid']
-                    questionsIds.append(questionid)
-                    k += 1
-            j += 1
+        if coursepageDTOList[i]['questionDTOList']:
+            j = 0
+            while j < len(coursepageDTOList[i]['questionDTOList']):
+                if coursepageDTOList[i]:
+                    questionsIds.append(coursepageDTOList[i]['questionDTOList'][j]['questionid'])
+                j += 1
         i += 1
     questionsIdList = list(set(questionsIds))
     l = 0
@@ -122,9 +121,6 @@ def check_answer(coursepageDTOList):
         get_answer_api = "https://api.ulearning.cn/questionAnswer/%d" % questionsIdList[l]
         response = requests.request(method='GET', url=get_answer_api, headers=headers)
         answerJson = response.json()
-        questions['questionid'] = answerJson['questionid']
-        questions['answerList'] = answerJson['correctAnswerList']
-        questions['score'] = 100
         if answerJson['subQuestionAnswerDTOList']:
             n = 0
             while n < len(answerJson['subQuestionAnswerDTOList']):
@@ -132,14 +128,77 @@ def check_answer(coursepageDTOList):
                 questions['answerList'] = answerJson['subQuestionAnswerDTOList'][n]['correctAnswerList']
                 questions['score'] = 100
                 print("第%d道大题的第%d小题答案获取完毕，准备提交..." % (l+1, n+1))
-                questionsData.append(questions)
+                questionsData.append(questions.copy())
                 n += 1
+        else:
+            questions['questionid'] = answerJson['questionid']
+            questions['answerList'] = answerJson['correctAnswerList']
+            questions['score'] = 100
+            questionsData.append(questions.copy())
         print("第%d道大题的答案获取完毕，准备提交..." % (l+1))
-        questionsData.append(questions)
         l += 1
     return questionsData
 
-def check_section(wholepageItemDTOList, userInfo):
+def speakingEnglish(coursepageDTOList, itemid, relationid):
+    global headers
+    print("检测到英语读单词")
+    status = int(input("0代表自动(会选择示范音，易封号)，1代表刷分(需要提前读一遍单词)，请选择模式："))
+    speak = []
+    speaks = {}
+    if status == 0:
+        i = 0
+        while i < len(coursepageDTOList):
+            if coursepageDTOList[i]['speak']:
+                j = 0
+                while j <len(coursepageDTOList[i]['speak']['audresp']):
+                    speaks['speakingid'] = coursepageDTOList[i]['speak']['audresp'][j]['audioid']
+                    speaks['score'] = 100
+                    speaks['time'] = random.randint(1,4)
+                    speaks['url'] = coursepageDTOList[i]['speak']['audresp'][j]['audiofileFullurl']
+                    direction = coursepageDTOList[i]['speak']['audresp'][j]['direction'].split()
+                    num = len(direction)
+                    if num > 1:
+                        k = 0
+                        str = "["
+                        while k < num:
+                            str += '{\\"%s\\":100}' % direction[k] + ","
+                            k += 1
+                        str = str.rstrip(",")
+                        str += "]"
+                        speaks['answer'] = str
+                    else:
+                        speaks['answer'] = "[{\"%s\":100}]" % direction[0]
+                    speak.append(speaks.copy())
+                    j += 1
+            i += 1
+
+    if status == 1:
+        api = "https://api.ulearning.cn/studyrecord/item/%s?courseType=4" % itemid
+        response = requests.request(method='GET', url=api, headers=headers)
+        res = response.json()
+        i = 0
+        while i < len(res['pageStudyRecordDTOList']):
+            if res['pageStudyRecordDTOList'][i]['pageid'] == relationid and res['pageStudyRecordDTOList'][i]['speaks'] and res['pageStudyRecordDTOList'][i]['studyTime'] != 0:
+                j = 0
+                while j < len(res['pageStudyRecordDTOList'][i]['speaks']):
+                    if res['pageStudyRecordDTOList'][i]['speaks'][j]['url']:
+                        speaks['speakingid'] = res['pageStudyRecordDTOList'][i]['speaks'][j]['speakingid']
+                        speaks['score'] = 100
+                        speaks['time'] = res['pageStudyRecordDTOList'][i]['speaks'][j]['time']
+                        speaks['url'] = res['pageStudyRecordDTOList'][i]['speaks'][j]['url']
+                        speaks['answer'] = re.sub(r'\d+', "100", res['pageStudyRecordDTOList'][i]['speaks'][j]['answer'])
+                        speak.append(speaks.copy())
+                    else:
+                        print("检测到该题未找到音频，将不会上传")
+                    j += 1
+            i += 1
+    if speak:
+        return speak
+    else:
+        print("程序故障")
+        return False
+
+def check_section(wholepageItemDTOList, userInfo, itemsInfo):
     global headers
     cryptoJsonData = {
         'itemid' : wholepageItemDTOList['itemid'],
@@ -151,10 +210,18 @@ def check_section(wholepageItemDTOList, userInfo):
         'score' : 100,
         'pageStudyRecordDTOList' : []
     }
-    get_item_title_api = "https://api.ulearning.cn/studyrecord/item/%s" % wholepageItemDTOList['itemid']
-    response = requests.request(method='GET', url=get_item_title_api, headers=headers)
-    title = response.json()['activity_title']
-    studyTime = int(input("请输入本章的%s小节需要的时间(以秒为单位，建议在400-1000，数据过大易封号，时间是叠加的，为0则不计入时长)：" % title))
+    if itemsInfo:
+        title = itemsInfo['title']
+    else:
+        try:
+            get_item_title_api = "https://api.ulearning.cn/studyrecord/item/%s?courseType=4" % wholepageItemDTOList[
+                'itemid']
+            response = requests.request(method='GET', url=get_item_title_api, headers=headers)
+            title = response.json()['activity_title']
+        except:
+            title = "未获取到标题"
+
+    studyTime = int(input("请输入本章的%s小节需要的时间(以秒为单位，建议在400-1000，数据过大易封号，时间是叠加的，为0则时间保持不变，负数则时间回退)：" % title))
     temple = {
         'pageid' : 0,
         'complete' : 1,
@@ -194,6 +261,11 @@ def check_section(wholepageItemDTOList, userInfo):
         if wholepageItemDTOList['wholepageDTOList'][i]['contentType'] == 5:
             pass
 
+        #英语读单词
+        if wholepageItemDTOList['wholepageDTOList'][i]['content'] == 'Speaking':
+            speaks = speakingEnglish(wholepageItemDTOList['wholepageDTOList'][i]['coursepageDTOList'], wholepageItemDTOList['itemid'], wholepageItemDTOList['wholepageDTOList'][i]['relationid'])
+            temple['speaks'] = speaks
+
         # 测验型
         if wholepageItemDTOList['wholepageDTOList'][i]['contentType'] == 7:
             questions = check_answer(wholepageItemDTOList['wholepageDTOList'][i]['coursepageDTOList'])
@@ -219,19 +291,19 @@ if __name__ == "__main__":
     print("欢迎使用优学院刷课脚本， 工具只是辅助作用切勿用来盈利，一切后果与作者无关")
     webbrowser.open("https://github.com/Chirmis")
     try:
-        f = open('cookies.txt')
+        f = open('./cookies.txt')
         f.close()
     except FileNotFoundError:
         # 创建空白文件
-        open('cookies.txt', 'w')
+        open('./cookies.txt', 'w')
     except PermissionError:
         exit("You don't have permission to access this file")
-    size = os.path.getsize('cookies.txt')
+    size = os.path.getsize('./cookies.txt')
     if size == 0:
         print("开始登陆....")
         login()
     else:
-        with open('cookies.txt', 'r') as f:
+        with open('./cookies.txt', 'r') as f:
             s = f.read()
         Token = dict(json.loads(s))
 
@@ -248,6 +320,8 @@ if __name__ == "__main__":
         print("课程编号：%d 课程名称：%s" % (i, resJson[i]['name']))
         i += 1
     coursesId = int(input("请选择课程编号："))
+    if coursesId > total-1:
+        exit("你哪只眼睛看到了有这个编号？？？")
     print("当前课程编号为：%d; 课程名称：%s" % (coursesId, resJson[coursesId]['name']))
     print("开始获取课程章节....")
     course_chapters_list_data = get_course_chapters_list(resJson[coursesId]['id'])
@@ -261,6 +335,10 @@ if __name__ == "__main__":
         chooseTextbookId = 0
     else:
         chooseTextbookId = int(input("请输入小课程编号:"))
+
+    if chooseTextbookId > textbookNum-1:
+        exit("你哪只眼睛看到了有这个编号？？？")
+
     courseName = course_chapters_list_data['textbook'][chooseTextbookId]['courseName']
     print("当前所选课程编号为：%d，课程名为%s" % (chooseTextbookId,courseName))
     textbookId = course_chapters_list_data['textbook'][chooseTextbookId]['courseId']
@@ -272,20 +350,36 @@ if __name__ == "__main__":
     print("%s下共有%d章节" % (coursename, chaptersNum))
     print("是否开启刷全章，开启输入1，不开启输入0(为了安全起见建议不要全章节刷)")
     status = int(input("请选择："))
-    if status:
+    if status == 1:
         k = 0
         while k < chaptersNum:
             print("当前选章节编号为%d，章节名称为：%s" % (k, chapters_item_dict['chapters'][k]['nodetitle']))
             print("正在获取%s章节下所有小节内容" % chapters_item_dict['chapters'][k]['nodetitle'])
-            get_every_section(chapters_item_dict['chapters'][k]['nodeid'])
+            get_every_section(chapters_item_dict['chapters'][k]['nodeid'],chapters_item_dict['chapters'][k]['items'])
             k += 1
-    else:
+    elif status == 0:
         k = 0
         while k < chaptersNum:
             print("章节编号%d，章节名称：%s" % (k, chapters_item_dict['chapters'][k]['nodetitle']))
             k += 1
-        chapterNumber = int(input("请选择章节编号："))
-        print("当前选章节编号为%d，章节名称为：%s" % (chapterNumber, chapters_item_dict['chapters'][chapterNumber]['nodetitle']))
-        print("正在获取%s章节下所有小节内容" % chapters_item_dict['chapters'][chapterNumber]['nodetitle'])
-        get_every_section(chapters_item_dict['chapters'][chapterNumber]['nodeid'])
+        str = input("请选择章节编号，多个章节请用英文逗号,隔开：")
+        strList = list(str.split(','))
+        i = 0
+        chapterList = []
+        while i < len(strList):
+            if strList[i] and strList[i].isdigit():
+                chapterList.append(strList[i])
+            else:
+                print("该项没有合法章节编号或分隔符使用错误。已被过滤")
+            i += 1
+        j = 0
+        while j < len(chapterList):
+            if int(chapterList[j]) <= chaptersNum:
+                chapterList[j] = int(chapterList[j])
+                print("当前选章节编号为%d，章节名称为：%s" % (chapterList[j], chapters_item_dict['chapters'][chapterList[j]]['nodetitle']))
+                print("正在获取%s章节下所有小节内容" % chapters_item_dict['chapters'][chapterList[j]]['nodetitle'])
+                get_every_section(chapters_item_dict['chapters'][chapterList[j]]['nodeid'],chapters_item_dict['chapters'][chapterList[j]]['items'])
+            j += 1
         exit("本次任务执行完毕")
+    else:
+        exit("输入有误，程序结束")
